7e839986d1da5d3df9703604811a450a2c39ff31
giordy <gio.andreola@stud.uniroma3.it>
2023-10-28 20:22:32 +0200

    gne2

--- src/main/java/ingegneria_dei_dati/Main.java
+++ src/main/java/ingegneria_dei_dati/Main.java
@@ -10,13 +10,13 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         JsonHandler handler = new JsonHandler("tables.json", Table.class);
-        System.out.println("Hello world!");
         int i=0;
         while (handler.hasNext()) {
             i+=1;
             if (i==5) break;
             Table t = (Table) handler.readNext();
             handler.readNext();
+
             System.out.println(Arrays.toString(t.headersCleaned));
             System.out.println(t.maxDimensions.row);
 
