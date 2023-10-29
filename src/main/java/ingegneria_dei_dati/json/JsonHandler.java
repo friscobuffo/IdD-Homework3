19ede2c81a5b8b8c5589ea3447f6834748231bea
giordy <gio.andreola@stud.uniroma3.it>
2023-10-29 13:32:57 +0100

    gne3

--- src/main/java/ingegneria_dei_dati/json/JsonHandler.java
+++ src/main/java/ingegneria_dei_dati/json/JsonHandler.java
@@ -5,10 +5,13 @@ import com.google.gson.Gson;
 import java.io.*;
 
 public class JsonHandler {
-    private final Class<?> c;
+    private final Class<?> c; // gli elementi json vengono convertiti in istanze di questa classe
     private String nextLine;
-    private final BufferedReader reader;
-    private final Gson gson;
+    private final BufferedReader reader; // il file json contiene un elemento (ovvero una tabella)
+    // per ogni riga, quindi possiamo scorrere il file con il BufferedReader e parsare una riga alla volta
+    private final Gson gson; // serve a convertire l'elemento del file json (passandolo in input
+    // come stringa) in un oggetto java
+
     public JsonHandler(String path, Class<?> c) throws IOException {
         this.c = c;
         this.reader = new BufferedReader(new FileReader(path));
